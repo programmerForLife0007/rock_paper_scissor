2750af8d31b464b26cf4c17c78b7746330642056
programmreForLife007 <programmrForLife007@protonmail.com>
2022-05-16 16:27:56 +0530

    Get random moves from computer

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello, World")
+function computerPlay() {
+    const moves = ["Rock", "Paper", "Scissors"];
+    function getRandomInteger(min, max) {
+        return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + min);
+    };
+    return moves[getRandomInteger(0,2)];
+}
+
+console.log(computerPlay())
+
