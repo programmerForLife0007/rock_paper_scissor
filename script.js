545292ba08abbceb294e8b89d6702465ee9df7da
programmreForLife007 <programmrForLife007@protonmail.com>
2022-05-16 17:32:34 +0530

    Setup condition for determinig the game

--- script.js
+++ script.js
@@ -3,8 +3,30 @@ function computerPlay() {
     function getRandomInteger(min, max) {
         return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + min);
     };
-    return moves[getRandomInteger(0,2)];
+    return moves[getRandomInteger(0, 2)];
 }
 
 console.log(computerPlay())
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection===computerSelection){
+        return "Draw";
+    }
+    else if (playerSelection>computerSelection){
+        if(computerSelection===1){
+            return "You Win"
+        }else{
+            if(playerSelection===2){return "You Lose"}else{return "You Win"}
+        }
+    }
+    else {
+        if(playerSelection===1){
+            return "You Lose"
+        }else{
+            if(computerSelection===1){
+                return "You Lose"
+            }else{return "You Win"}
+        }
+    }
+}
+console.log(playRound(0,0));
