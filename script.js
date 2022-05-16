b18a98d1c8ac10ac5c739bb550e26807f12cd6c5
programmreForLife007 <programmrForLife007@protonmail.com>
2022-05-16 21:02:15 +0530

    Finish v1

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 const moves = ["rock", "paper", "scissors"];
 
+
 function computerPlay() {
     return Math.floor(Math.random() * (Math.floor(2) - Math.ceil(0) + 1) + 0);
 };
 
-
 function playRound(pSelect, cSelect) {
     if (pSelect === cSelect) {
-        return "Draw";
+        return score = score + 0;
     }
     else if (pSelect > cSelect) {
         if (cSelect === 1) {
-            return "You Win"
+            return score++
         } else {
-            return pSelect === 2 ? "You Lose" : "You Win"
+            return pSelect === 2 ? score-- : score++
         }
     }
     else {
         if (pSelect === 1) {
-            return "You Lose"
+            return score--
         } else {
-            return cSelect === 1 ? "You Lose" : "You Win"
+            return cSelect === 1 ? score-- : score++
         }
     }
 }
 
 
-for (let i = 0; i < 5; i++) {
-    // your code here!
-    const pSelect = moves.indexOf("Rock");
-    const cSelect = computerPlay();
-    playRound(pSelect, cSelect)
-    console.log(playRound(pSelect, cSelect));
-    
-}
 let score = 0;
+
+for (let i = 0; i < 5; i++) {
+    let userPlay = prompt("Rock, Paper, Scissors").toLowerCase();
+    const pSelect = moves.indexOf(userPlay);
+    const cSelect = computerPlay();
+    playRound(pSelect, cSelect);
+    console.log(score);
+}
+
+
+
+(score === 0) ? console.log("It's Draw")
+    : (score > 0) ? console.log("You Win")
+        : console.log("You Lose")
